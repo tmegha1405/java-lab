4184bcc481d1d5bf14537bfba56d8b996ef6e583
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2023-05-15 11:28:28 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+class Rectangle {
+double length,breadth;
+public void setData()
+{
+length=12.48;
+breadth=13;
+}
+public void getArea()
+{
+double area= length*breadth;
+System.out.println("Area of rectangle is "+area);
+}
+public static void main(String args[]){
+Rectangle obj=new Rectangle();
+obj.setData();
+obj.getArea();
+}
+}
---
+++ WelcomeJava.java
@@ -0,0 +1,7 @@
+public class WelcomeJava 
+{
+public static void main(String[] args) 
+{
+System.out.println("Weclome Java");
+}
+}
